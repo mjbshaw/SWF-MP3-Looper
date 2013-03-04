678824484790d36a65a675fa9bab72971ac8bb7f
Michael Bradshaw <mjbshaw@gmail.com>
2013-03-03 21:40:23 -0700

    Add audio decoder initialization

--- SWF MP3 Looper/AudioDecoder.cpp
+++ SWF MP3 Looper/AudioDecoder.cpp
@@ -7,6 +7,45 @@ extern "C"
 #include <libswresample/swresample.h>
 }
 
-AudioDecoder::AudioDecoder()
+#include <new>
+#include <stdexcept>
+
+AudioDecoder::AudioDecoder() : audioStream(nullptr),
+	frame(nullptr, av_free),
+	codec(nullptr, avcodec_close),
+	format(nullptr, [](AVFormatContext* f) { avformat_close_input(&f); }),
+	swr(nullptr, [](SwrContext* s) { swr_free(&s); })
 {
+	frame.reset(avcodec_alloc_frame());
+	if (!frame)
+	{
+		throw std::bad_alloc();
+	}
+
+	AVFormatContext* ctx = NULL;
+	if (avformat_open_input(&ctx, "path", nullptr, nullptr) != 0)
+	{
+		throw std::runtime_error("Error opening the file");
+	}
+	format.reset(ctx);
+
+	if (avformat_find_stream_info(format.get(), NULL) < 0)
+	{
+		throw std::runtime_error("Error finding the stream info");
+	}
+
+	AVCodec* cdc = NULL;
+	int stream = av_find_best_stream(format.get(), AVMEDIA_TYPE_AUDIO, -1, -1, &cdc, 0);
+	if (stream < 0)
+	{
+		throw std::runtime_error("No good audio stream");
+	}
+	audioStream = format->streams[stream];
+
+	audioStream->codec->codec = cdc;
+	if (avcodec_open2(audioStream->codec, audioStream->codec->codec, NULL) != 0)
+	{
+		throw std::runtime_error("Couldn't open the context with the decoder");
+	}
+	codec.reset(audioStream->codec);
 }
--- SWF MP3 Looper/AudioDecoder.hpp
+++ SWF MP3 Looper/AudioDecoder.hpp
@@ -1,13 +1,17 @@
 #ifndef AUDIO_DECODER_HPP
 #define AUDIO_DECODER_HPP
 
+extern "C"
+{
 struct AVFrame;
 struct AVFormatContext;
 struct AVStream;
 struct AVCodecContext;
 struct SwrContext;
+}
 
 #include <memory>
+#include <functional>
 
 class AudioDecoder
 {
@@ -18,8 +22,13 @@ private:
 	AudioDecoder(const AudioDecoder&) {}
 	AudioDecoder& operator=(const AudioDecoder&) {return *this;}
 
+	AVStream* audioStream;
 	// Order of these members is important (so that they are destroyed in the right order)
-	std::unique_ptr<AVFrame> frame;
+	std::unique_ptr<AVFrame, std::function<void(void*)>> frame;
+	std::unique_ptr<AVCodecContext, std::function<int(AVCodecContext*)>> codec;
+	std::unique_ptr<AVFormatContext, std::function<void(AVFormatContext*)>> format;
+	std::unique_ptr<SwrContext, std::function<void(SwrContext*)>> swr;
+
 };
 
 #endif
